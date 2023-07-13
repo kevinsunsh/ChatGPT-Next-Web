ae2faf130eb1f093895bc17fc92c36af56308916
Kevin Sun <sunzhipeng8@gmail.com>
2023-07-13 14:12:17 +0800

    remove useless file

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,6 @@ export default function RootLayout({
       <head>
         <meta name="config" content={JSON.stringify(getClientConfig())} />
         <link rel="manifest" href="/site.webmanifest"></link>
-        <script src="/serviceWorkerRegister.js" defer></script>
       </head>
       <body>{children}</body>
     </html>
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 import webpack from "webpack";
 
-const mode = process.env.BUILD_MODE ?? "standalone";
-console.log("[Next] build mode", mode);
-
-const disableChunk = !!process.env.DISABLE_CHUNK || mode === "export";
+const disableChunk = !!process.env.DISABLE_CHUNK;
 console.log("[Next] build with chunk: ", !disableChunk);
 
 /** @type {import('next').NextConfig} */
@@ -26,66 +23,9 @@ const nextConfig = {
 
     return config;
   },
-  output: mode,
   images: {
-    unoptimized: mode === "export",
+    unoptimized: true,
   },
 };
-
-if (mode !== "export") {
-  nextConfig.headers = async () => {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "*",
-          },
-          {
-            key: "Access-Control-Max-Age",
-            value: "86400",
-          },
-        ],
-      },
-    ];
-  };
-
-  nextConfig.rewrites = async () => {
-    const ret = [
-      {
-        source: "/api/proxy/:path*",
-        destination: "https://api.openai.com/:path*",
-      },
-      {
-        source: "/google-fonts/:path*",
-        destination: "https://fonts.googleapis.com/:path*",
-      },
-      {
-        source: "/sharegpt",
-        destination: "https://sharegpt.com/api/conversations",
-      },
-    ];
-
-    const apiUrl = process.env.API_URL;
-    if (apiUrl) {
-      console.log("[Next] using api url ", apiUrl);
-      ret.push({
-        source: "/api/:path*",
-        destination: `${apiUrl}/:path*`,
-      });
-    }
-
-    return {
-      beforeFiles: ret,
-    };
-  };
-}
 
 export default nextConfig;
